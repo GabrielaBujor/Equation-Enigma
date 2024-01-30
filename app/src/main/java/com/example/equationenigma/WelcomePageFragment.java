79fe0715381efb1bce1668f23bb216101f650d34
BujorGabriela <gbujor4@gmail.com>
2024-01-30 22:16:14 +0200

    Added special fonts

--- app/src/main/java/com/example/equationenigma/WelcomePageFragment.java
+++ app/src/main/java/com/example/equationenigma/WelcomePageFragment.java
@@ -1,7 +1,11 @@
 package com.example.equationenigma;
 
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
+
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,6 +30,11 @@ public class WelcomePageFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_welcome_page, container, false);
         ImageView imageView = view.findViewById(R.id.imageView);
         TextView textViewDescription = view.findViewById(R.id.textViewDescription);
+
+        Typeface typeface = ResourcesCompat.getFont(getContext(), R.font.playfairdisplay_black);
+        textViewDescription.setTypeface(typeface);
+
+        imageView.animate().scaleX(1.1f).scaleY(1.1f).setDuration(500).start();
 
         imageView.setImageResource(imageResourceId);
         textViewDescription.setText(textDescription);
