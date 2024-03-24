c5feafabae4748d3959cad81ddb9adfe3dacfb04
BujorGabriela <gbujor4@gmail.com>
2024-03-24 20:16:17 +0200

    Added fragments v2

--- app/src/main/java/com/example/equationenigma/MainActivity.java
+++ app/src/main/java/com/example/equationenigma/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.equationenigma;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.equationenigma.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    replaceFragment(new HomeFragment());
+                    break;
+
+                case R.id.navigation_quiz:
+                    replaceFragment(new QuizFragment());
+                    break;
+
+                case R.id.navigation_report:
+                    replaceFragment(new ReportFragment());
+                    break;
+
+                case R.id.navigation_profile:
+                    replaceFragment(new UserProfileFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
